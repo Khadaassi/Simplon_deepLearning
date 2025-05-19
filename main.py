4678dfbfa9554bffd055336bc51025162da27513
Khadaassi <aassikhadija@yahoo.fr>
2025-05-19 16:24:00 +0200

    change directory name.

---
+++ main.py
@@ -0,0 +1,3 @@
+from utils.load_data import load_data
+
+data = load_data("data/WA_Fn-UseC_-Telco-Customer-Churn.csv")
---
+++ utils/preprocessor.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+
+def preprocess_data(df: pd.DataFrame):
+    pass
+
