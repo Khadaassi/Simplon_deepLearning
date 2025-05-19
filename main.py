7aeb15c3755a04536a15452fad8d900671e65653
Khadaassi <aassikhadija@yahoo.fr>
2025-05-19 16:37:21 +0200

    add import and base for main.py

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from utils.load_data import load_data
+from utils.preprocessor import preprocess_data
+from utils.build_pipeline import *
+from utils.metrics  import *
 
 data = load_data("data/WA_Fn-UseC_-Telco-Customer-Churn.csv")
+
+def main():
+
+    pass
+
+if __name__ == "__main__":
+    main()
