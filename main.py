375de6feec2d289905a5da082613b4a10c2e5b6d
Wael Bensoltana <wael.bensoltana@gmail.com>
2025-05-20 10:09:36 +0200

    build pipeline

--- main.py
+++ main.py
@@ -7,7 +7,17 @@ data = load_data("data/WA_Fn-UseC_-Telco-Customer-Churn.csv")
 
 def main():
 
-    pass
+    original_data = preprocess_data(data)
+    #print(original_data.shape)
+    [X_train_preprocessed, X_val_preprocessed, X_test_preprocessed, y_train, y_val, y_test] = build_pipeline(original_data)
+    model = build_model(X_train_preprocessed, X_val_preprocessed,y_train, y_val )
+    ROC, F1, Recall = metrics(model, X_test_preprocessed, y_test)
+   """ print(X_train_preprocessed.shape)
+    print(X_val_preprocessed.shape)
+    print(X_test_preprocessed.shape)
+    print(y_train.shape)
+    print(y_val.shape)
+    print(y_test.shape)"""
 
 if __name__ == "__main__":
     main()
